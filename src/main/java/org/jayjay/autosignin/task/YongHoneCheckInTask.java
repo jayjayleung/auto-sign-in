b3caebdecd88bd3849399ff659bbe888dffd21ff
liangshijie <liangshijie@mrcdevlab.com>
2025-04-10 10:22:38 +0800

    yh update

--- src/main/java/org/jayjay/autosignin/task/YongHoneCheckInTask.java
+++ src/main/java/org/jayjay/autosignin/task/YongHoneCheckInTask.java
@@ -39,6 +39,105 @@ public class YongHoneCheckInTask extends CheckInTask {
         return new MessageList("YongHong 签到", listMessage);
     }
 
+//    public static void main(String[] args) {
+//        System.out.println("yonghong 签到任务开始");
+//        String yhUsername = System.getenv("YH_USERNAME");
+//        String yhPassword = System.getenv("YH_PASSWORD");
+//        if (StrUtil.isBlank(yhUsername) || StrUtil.isBlank(yhPassword)) {
+//            System.out.println("yonghong 账号密码未配置，跳过签到");
+//            return;
+//        }
+//        System.out.println("yhUsername: " + yhUsername);
+//        System.out.println("yhPassword: " + yhPassword);
+//
+//        //自动下载，第一次下载后不会再下载
+//        RevisionInfo revisionInfo = null;
+//        try {
+//            revisionInfo = Puppeteer.downloadBrowser();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("revisionInfo: " + revisionInfo);
+//        ArrayList<String> argList = new ArrayList<>();
+//        // withHeadless 是否开启无头模式，无头模式不会显示浏览器
+////        LaunchOptions options = new LaunchOptionsBuilder().withArgs(argList).withHeadless(false).build();
+//        argList.add("--no-sandbox");
+//        argList.add("--disable-setuid-sandbox");
+////        argList.add("--user_agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82\"");
+//        LaunchOptions options = LaunchOptions.builder()
+//                .args(argList)
+//                .defaultViewport(new Viewport(1900, 1080))
+//                .headless(false)
+//                .protocol(Protocol.CDP)
+//                .product(Product.Chrome)
+//                .build();
+//        try (Browser browser = Puppeteer.launch(options)) {
+//            System.out.println(browser.userAgent());
+//            Page page = browser.newPage();
+//            page.setUserAgent(userAgent);
+//            page.goTo("https://club.yonghongtech.com/member.php?mod=logging&action=login&phonelogin=no");
+//            ElementHandle userName = page.$("input[name='username']");
+//            userName.type(yhUsername);
+//            page.type("input[name='password']", yhPassword);
+//            ElementHandle loginBtn = page.$("button[name='loginsubmit']");
+//            System.out.println("点击登录");
+//            loginBtn.click();
+//            loginBtn.dispose();
+//            Thread.sleep(5000);
+//            System.out.println(page.cookies());
+//            Optional<Cookie> any = page.cookies().stream().filter(cookie -> "user_id".equalsIgnoreCase(cookie.getName())).findAny();
+//            if (any.isPresent()) {
+//                String uid = any.get().getValue();
+//                System.out.println("获取cookie成功:" + uid);
+//                System.out.println(page.url());
+//                String url = "https://club.yonghongtech.com/home.php?mod=space&uid=" + any.get().getValue() + "&do=signlog&from=space";
+//                page.goTo(url);
+//                Thread.sleep(5000);
+//                System.out.println(page.url());
+//                Document document = Jsoup.parse(page.content());
+//                Elements me = document.select(".nex_Home_intel h5");
+//                if (me != null) {
+//                    String user = me.text().replaceAll(MessageUtil.lineEnd, "");
+//                    System.out.println(user);
+//                }
+//
+//                // document.querySelector("a[title='个人设置'].innerText")
+//                //有bug,老是找不到节点
+////                    page.hover(".hl_member_avator");
+////                    page.waitForSelector("a[title='个人设置']");
+////                    Object evaluate = page.$eval("a[title='个人设置']", "ele=>ele.innerText.replaceAll('\\n','')");
+////                    addMessage(lineMsg("用户名:").append(evaluate));
+////                    addMessage(lineMsg("洪豆:").append(page.$eval(".hl_member_in_status", "ele=>ele.innerText.replaceAll('\\n','')")));
+////                Page card = browser.newPage();
+////                card.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82");
+////                String url = "https://club.yonghongtech.com/home.php?mod=space&uid=" + any.get().getValue() + "&do=signlog&from=space";
+////                System.out.println("card_url:"+url);
+////                page.goTo(url);
+////                System.out.println("进入打卡页面");
+//                Thread.sleep(1000);
+//                System.out.println("开始论坛发布");
+//                page = browser.newPage();
+//                page.setUserAgent(userAgent);
+//                page.goTo("https://club.yonghongtech.com/thread-110640-1-1.html");
+//                Thread.sleep(5000);
+//
+//                ElementHandle fastpostmessage = page.$("#fastpostmessage");
+//                fastpostmessage.type("签到");
+//                ElementHandle fastpostsubmit = page.$("#fastpostsubmit");
+//                fastpostsubmit.click();
+//                System.out.println("-------------------");
+//            } else {
+//                System.out.println("获取cookie失败");
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     @Test
     public void testRun() {
         run();
@@ -209,10 +308,13 @@ public class YongHoneCheckInTask extends CheckInTask {
                 } else {
                     flag = true;
                     System.out.println(page.url());
+                    break;
                 }
             }
             if (flag) {
+                Thread.sleep(2000);
                 page.$("#fastpostmessage").type("签到");
+                Thread.sleep(2000);
                 page.$("#fastpostsubmit").click();
                 addMessage("发布完成，洪豆+25");
             }
