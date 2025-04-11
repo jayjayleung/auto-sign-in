da7fb5f2760576274feee265cb6131dede0bcf5e
liangshijie <liangshijie@mrcdevlab.com>
2025-04-11 10:03:01 +0800

    yh update

--- src/main/java/org/jayjay/autosignin/task/YongHoneCheckInTask.java
+++ src/main/java/org/jayjay/autosignin/task/YongHoneCheckInTask.java
@@ -313,9 +313,11 @@ public class YongHoneCheckInTask extends CheckInTask {
             }
             if (flag) {
                 Thread.sleep(2000);
-                page.$("#fastpostmessage").type("签到");
-                Thread.sleep(2000);
-                page.$("#fastpostsubmit").click();
+//                page.$("#fastpostmessage").type("签到");
+//                Thread.sleep(2000);
+//                page.$("#fastpostsubmit").click();
+                page.evaluate("document.getElementById('fastpostmessage').innerText='签到'");
+                page.evaluate("document.getElementById('fastpostsubmit').click();");
                 addMessage("发布完成，洪豆+25");
             }
             System.out.println("论坛发布完成");
